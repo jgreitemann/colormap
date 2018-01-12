78cfd6994850a8e7fb06512cb7f396f8cef9153d
Jonas Greitemann <j.greitemann@lmu.de>
2018-01-12 13:23:01 +0100

    Changed map_iterator_adapter to keep BaseIterator as a member
    STL iterators should not be inherited from as their dtors are non-virtual.
    map_iterator_adapter uses BaseIterator's iterator_category tag as its own and
    enables the appropriate features via type traits.

--- map_iterator_adapter.hpp
+++ map_iterator_adapter.hpp
@@ -1,28 +1,139 @@
 #pragma once
 
+#include <iterator>
 #include <memory>
+#include <type_traits>
 #include <utility>
 
 
 namespace itadpt {
 
-    template <typename BaseIterator, typename Functor>
-    struct map_iterator_adapter : public BaseIterator {
+    template <typename BaseIterator, typename Functor,
+              typename = typename std::enable_if<std::is_base_of<std::input_iterator_tag, typename std::iterator_traits<BaseIterator>::iterator_category>::value>::type>
+    struct map_iterator_adapter {
     private:
+        BaseIterator base;
         Functor & functor;
     public:
-        typedef typename BaseIterator::value_type domain_type;
+        typedef typename std::iterator_traits<BaseIterator>::value_type domain_type;
         typedef decltype(functor(domain_type())) value_type;
+        typedef typename std::iterator_traits<BaseIterator>::difference_type difference_type;
+        typedef value_type reference;
+        typedef std::unique_ptr<value_type> pointer;
+        typedef typename std::iterator_traits<BaseIterator>::iterator_category iterator_category;
 
         map_iterator_adapter (BaseIterator it, Functor & f)
-            : BaseIterator(it), functor(f) {}
+            : base(it), functor(f) {}
 
-        value_type operator* () const {
+        reference operator* () const {
             return functor(**((BaseIterator *)this));
         }
 
-        std::unique_ptr<value_type> const operator-> () const {
-            return std::unique_ptr<value_type>(new value_type(functor(**((BaseIterator *)this))));
+        pointer operator-> () const {
+            return pointer(new value_type(functor(**((BaseIterator *)this))));
+        }
+
+        friend bool operator== (map_iterator_adapter const& lhs, map_iterator_adapter const& rhs) {
+            return lhs.base == rhs.base;
+        }
+
+        friend bool operator!= (map_iterator_adapter const& lhs, map_iterator_adapter const& rhs) {
+            return lhs.base != rhs.base;
+        }
+
+        map_iterator_adapter & operator++ () {
+            base++;
+            return *this;
+        }
+
+        template <typename Tag = iterator_category,
+                  typename = typename std::enable_if<std::is_base_of<std::forward_iterator_tag, Tag>::value>::type>
+        map_iterator_adapter operator++ (int) {
+            map_iterator_adapter old(*this);
+            ++(*this);
+            return old;
+        }
+
+        template <typename Tag = iterator_category,
+                  typename = typename std::enable_if<std::is_base_of<std::bidirectional_iterator_tag, Tag>::value>::type>
+        map_iterator_adapter & operator-- () {
+            base--;
+            return *this;
+        }
+
+        template <typename Tag = iterator_category,
+                  typename = typename std::enable_if<std::is_base_of<std::bidirectional_iterator_tag, Tag>::value>::type>
+        map_iterator_adapter operator-- (int) {
+            map_iterator_adapter old(*this);
+            --(*this);
+            return old;
+        }
+
+        template <typename Tag = iterator_category,
+                  typename = typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, Tag>::value>::type>
+        map_iterator_adapter & operator+= (difference_type i) {
+            base += i;
+            return *this;
+        }
+
+        template <typename Tag = iterator_category,
+                  typename = typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, Tag>::value>::type>
+        map_iterator_adapter & operator-= (difference_type i) {
+            return *this += -i;
+        }
+
+        template <typename Tag = iterator_category,
+                  typename = typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, Tag>::value>::type>
+        friend map_iterator_adapter operator+ (map_iterator_adapter it, difference_type i) {
+            return it += i;
+        }
+
+        template <typename Tag = iterator_category,
+                  typename = typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, Tag>::value>::type>
+        friend map_iterator_adapter operator+ (difference_type i, map_iterator_adapter const& it) {
+            return it + i;
+        }
+
+        template <typename Tag = iterator_category,
+                  typename = typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, Tag>::value>::type>
+        friend map_iterator_adapter operator- (map_iterator_adapter const& it, difference_type i) {
+            return it + (-i);
+        }
+
+        template <typename Tag = iterator_category,
+                  typename = typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, Tag>::value>::type>
+        friend difference_type operator- (map_iterator_adapter const& lhs, map_iterator_adapter const& rhs) {
+            return lhs.base - rhs.base;
+        }
+
+        template <typename Tag = iterator_category,
+                  typename = typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, Tag>::value>::type>
+        reference operator[] (difference_type i) {
+            return *(*this + i);
+        }
+
+        template <typename Tag = iterator_category,
+                  typename = typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, Tag>::value>::type>
+        friend bool operator< (map_iterator_adapter const& lhs, map_iterator_adapter const& rhs) {
+            return lhs.base < rhs.base;
+        }
+
+        template <typename Tag = iterator_category,
+                  typename = typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, Tag>::value>::type>
+        friend bool operator> (map_iterator_adapter const& lhs, map_iterator_adapter const& rhs) {
+            return lhs.base > rhs.base;
+        }
+
+        template <typename Tag = iterator_category,
+                  typename = typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, Tag>::value>::type>
+        friend bool operator<= (map_iterator_adapter const& lhs, map_iterator_adapter const& rhs) {
+            return lhs.base <= rhs.base;
+        }
+
+        template <typename Tag = iterator_category,
+                  typename = typename std::enable_if<std::is_base_of<std::random_access_iterator_tag, Tag>::value>::type>
+        friend bool operator>= (map_iterator_adapter const& lhs, map_iterator_adapter const& rhs) {
+            return lhs.base >= rhs.base;
         }
     };
 
