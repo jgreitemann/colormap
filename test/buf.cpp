488b98510c9df040d9e5af91948bd81208c60642
Jonas Greitemann <j.greitemann@lmu.de>
2019-07-24 21:21:38 +0200

    Add simpler demo `test/buf.cpp`

---
+++ test/buf.cpp
@@ -0,0 +1,46 @@
+// colormap -- color palettes, map iterators, grids, and PPM export
+// Copyright (C) 2018-2019  Jonas Greitemann
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+#include "colormap.hpp"
+
+#include <array>
+#include <fstream>
+
+
+int main () {
+    // float buf[100];
+    float buf[100];
+    for (size_t i = 0; i < 100; ++i) {
+        buf[i] = i;
+    }
+
+    // get a colormap and rescale it
+    auto pal = color::palettes.at("inferno").rescale(0., 100.);
+    // and use it to map the values to colors
+    auto pix = itadpt::map(buf, pal);
+
+    // Construct a PPM object from the `mapped` object `pix`. Color space is
+    // inferred from color type of pix. "inferno" is an RGB palette, so `pmap`
+    // will represent a PPM image. For a grayscale colormap, it would result in
+    // a PGM image.
+    color::pixmap<decltype(pix.begin())> pmap(pix.begin(), std::make_pair(10, 10));
+
+    std::ofstream os("buf." + pmap.file_extension(),
+                     std::ios_base::binary);
+    pmap.write_binary(os);
+
+}
